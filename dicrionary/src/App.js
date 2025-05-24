d5734caf191cef5cbf51d6cecf74a176b44803b5
SZ-Aarica <112103155+SZ-Aarica@users.noreply.github.com>
2025-05-24 15:45:36 +0330

    dictionary app start

---
+++ dicrionary/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import Dict from './Dict';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Dictionary app
+
+      </header>
+      <Dict />
+    </div>
+  );
+}
+
+export default App;
---
+++ dicrionary/src/Dict.js
@@ -0,0 +1,11 @@
+function Structure(){
+
+    return(
+        <div>
+
+            <footer>
+                coded by Saba Zahedi
+            </footer>
+        </div>
+    )
+}export default Structure
